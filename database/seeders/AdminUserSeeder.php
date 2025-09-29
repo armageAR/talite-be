02a0cf31c24611fb557598a621a236bb167ee82c
Ariel Gerling <agerling@strivven.com>
2025-09-28 21:29:59 -0300

    seeder para usuario admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Evita duplicados si lo corrés varias veces
+        User::updateOrCreate(
+            ['email' => 'admin@teatro.local'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]
+        );
+    }
+}
